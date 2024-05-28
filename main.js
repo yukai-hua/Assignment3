a615050f9f446db17c33321e29057223d47942fe
Yukai Hua <yukhua@student.unimelb.edu.au>
2024-05-29 03:52:23 +1000

    modified code

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ function displayCart(){
         productContainer.innerHTML += `
         <div class="product">
             <div class="prod_detail">
-              <img src="${item.tag}.svg">
+              <img src="images/${item.tag}.svg">
               <div class="info">
                 <div class="name">${item.product}</div>
                 <small>Box size: 1 person (2 meals)</small>
@@ -140,17 +140,17 @@ function displayCart(){
       summarySection.innerHTML = `
       <div class="order-summary">
         <h2>Order Summary</h2>
-        <p class="subtotal">Recipe (  <span class="item_count">${productNumbers}</span>)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$<span class="price"> ${cartCost}</span></p>
-        <p class="delivery">Free Delivery &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$<span class="price">       0</span></p>
+        <p class="subtotal">Recipe (${productNumbers})<span class="price">$ ${cartCost}</span></p>
+        <p class="delivery">Free Delivery<span class="price">$0</span></p>
         <hr>
-        <p class="total">Total<span class="symbol">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$</span><span class="total_price">      ${cartCost}</span></p>
+        <p class="total">Total<span class="total_price">$${cartCost}</span></p>
         <div class="date-time">
           <div onclick="showDatePicker()">
-              <img src="Calendar.svg" alt="Calendar">
+              <img src="images/Calendar.svg" alt="Calendar">
               <span class="selected-date" id="selected-date">choose date and time</span>
           </div>
           <div class="dropdown">
-            <img src="clock.svg" alt="Clock">
+            <img src="images/clock.svg" alt="Clock">
             <select name="time" id="time-options" class="time-options">6:00~8:00
               <option>8:00~10:00</option>
               <option>10:00~12:00</option>
@@ -198,32 +198,13 @@ function decreaseQuantity(tag) {
     let cartCost = localStorage.getItem('totalCost');
     localStorage.setItem('totalCost', cartCost - cartItems[tag].price);
     displayCart();
-  }else{
-    let productContainer=document.querySelector(".product-container");
-    Object.values(cartItems).map(item =>{
-      productContainer.innerHTML -= `
-      <div class="product">
-          <div class="prod_detail">
-            <img src="${item.tag}.svg">
-            <div class="info">
-              <div class="name">${item.product}</div>
-              <small>Box size: 1 person (2 meals)</small>
-            </div>  
-          </div> 
-
-          <div class="item">
-            <button type="button" class="minus" id="minus">
-            </button>
-            <span class="Quantity">${item.inCart}</span>
-            <button type="button" class="add" id="add">
-            </button>
-          </div>
-          <span class="the-price">${item.inCart * item.price}</span>
-      </div>       
-      `
+    }else{
+      let productContainer=document.querySelector(".product-container");
+      Object.values(cartItems).map(item =>{
     });
-  }
-}  
+  }  
+}
+
 
 document.addEventListener('click', function(event) {
   if (event.target.classList.contains('add')) {
